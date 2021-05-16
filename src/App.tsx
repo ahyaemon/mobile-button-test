c89e4134a94829725959e74b46a4aaebbc5685ed
ahyaemon <iamahyaemon@gmail.com>
2021-05-16 20:40:36 +0900

    add button 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [s, setS] = useState('')
+        return (
+        <div className="App">
+            <h1>sample</h1>
+            <hr/>
+            <p>pressed button: {s}</p>
+            <div className="button"
+                onClick={() => { setS('1') }}
+            ></div>
+        </div>
+    );
 }
 
 export default App;
