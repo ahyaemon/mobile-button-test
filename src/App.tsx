64de6578e1c14146be7fa8c4e89242b44cd4f31f
ahyaemon <iamahyaemon@gmail.com>
2021-05-16 23:33:02 +0900

    add buttons

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,23 @@ function App() {
             <div className="button"
                 onClick={() => { setS('1') }}
             ></div>
+            <div className="button"
+                onClick={(e) => {
+                    e.preventDefault()
+                    setS('2')
+                }}
+            ></div>
+            <div className="button"
+                onTouchStart={() => {
+                    setS('3')
+                }}
+            ></div>
+            <div className="button"
+                onTouchStart={(e) => {
+                    e.preventDefault()
+                    setS('4')
+                }}
+            ></div>
         </div>
     );
 }
