b52d863d9d73ce6d9a8b5d974ca1b6c48dd4dc0a
ahyaemon <iamahyaemon@gmail.com>
2021-05-17 11:59:03 +0900

    update

--- src/App.tsx
+++ src/App.tsx
@@ -8,24 +8,38 @@ function App() {
             <h1>mobile button test</h1>
             <hr/>
             <p>pressed button: {s}</p>
-            <div className="button"
-                onClick={() => { setS('1') }}
+            <div
+                id="button1"
+                className="button"
+                onTouchStart={() => {
+                    setS('1')
+                }}
             ></div>
-            <div className="button"
-                onClick={(e) => {
+            <div
+                id="button2"
+                className="button"
+                onTouchStart={(e) => {
                     e.preventDefault()
                     setS('2')
                 }}
             ></div>
-            <div className="button"
-                onTouchStart={() => {
+            <div
+                id="button3"
+                className="button"
+                onTouchStart={(e) => {
+                    e.preventDefault()
                     setS('3')
                 }}
             ></div>
-            <div className="button"
+            <div
+                id="button4"
+                className="button"
                 onTouchStart={(e) => {
                     e.preventDefault()
                     setS('4')
+                }}
+                onContextMenu={(e) => {
+                    e.preventDefault()
                 }}
             ></div>
         </div>
